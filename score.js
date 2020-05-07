b008316acbcf967433853b41d06505c3ffa4bc6f
JishaChackoV <jishtechversant@gmail.com>
2020-05-07 12:35:52 +0530

    redirection change , timer clear

--- score.js
+++ score.js
@@ -1,6 +1,8 @@
+
+var playerScore = localStorage.getItem('score');
 document.getElementById('name').innerText = localStorage.getItem('playerName');
-document.getElementById('points').innerText = localStorage.getItem('score');
-document.getElementById('highestPoint').innerText = localStorage.getItem('score');
+document.getElementById('points').innerText = (playerScore && playerScore !== 0) ? localStorage.getItem('score') : 0;
+document.getElementById('highestPoint').innerText = (playerScore && playerScore !== 0) ? localStorage.getItem('score') : 0;
 
 function homeClick(){
     window.location.href='home.html';
--- scrample.js
+++ scrample.js
@@ -4,139 +4,140 @@ const wordslv1 = [
     "conference",
     "professional",
     "butterfly",
-   
-  ];
-  const wordslv2 = [
+
+];
+const wordslv2 = [
     "acknowledgment",
     "adversity",
     "capability",
     "capacious",
     "elliptical"
-  ]
-  
-  var correct = 0;
-  let score = 0;
-  var level = 1;
-  var randomData;
-  var myInterval;
-  
-  
-  function randomWord(words){
+]
+
+var correct = 0;
+let score = 0;
+var level = 1;
+var randomData;
+var myInterval;
+
+
+function randomWord(words) {
+    myStopFunction();
     timerFunction();
     randomData = words[Math.floor(Math.random() * words.length)];
-  }
-  
-  function shuffled(randomWord){
-      
-     let shuffledWord = randomWord.split('').sort(function(){return 0.5-Math.random()}).join('');
-      const wordSplit = [...shuffledWord] ;
-      let scrambleOutput = document.getElementById('scrambled-word');
-      scrambleOutput.innerHTML= `<table style="border:solid 1px; align-left:50%" ;width:40%><tr><td>${wordSplit}</td></tr><table>` ;
-      return  shuffledWord
-  }
+}
 
-  function clearField(){
+function shuffled(randomWord) {
+
+    let shuffledWord = randomWord.split('').sort(function () { return 0.5 - Math.random() }).join('');
+    const wordSplit = [...shuffledWord];
+    let scrambleOutput = document.getElementById('scrambled-word');
+    scrambleOutput.innerHTML = `<table style="border:solid 1px; pull-left:50%" ;width:40%><tr><td>${wordSplit}</td></tr><table>`;
+    return shuffledWord
+}
+
+function clearField() {
 
     document.getElementById('user-guess').value = "";
 
-  }
- 
-  function checkAnswer(){
+}
 
-      var userGuess = document.getElementById("user-guess").value; 
-      myStopFunction();
-      if(userGuess === randomData){
-       
-          correct += 1;
-          score = ((score + 5) * level);
-          if (correct >= 5){
-            
-              level += 1; 
-              myStopFunction();
-              correct = 0;
-              document.getElementById('level').innerText = level;
-              document.getElementById('info').innerHTML = "";
+function checkAnswer() {
 
-          }
-          localStorage.setItem("score",score);
-          document.getElementById('info').innerHTML = `<span style="color:green;" >CORRECT</span>`;
-          levelSetting();
-          clearField();
-          
-      }else{
+    var userGuess = document.getElementById("user-guess").value;
+    myStopFunction();
+    if (userGuess === randomData) {
 
-            info.innerHTML = `<span style="color:red";>WRONG</span>`;
-            clearField();  
-            levelSetting()
+        correct += 1;
+        score = ((score + 5) * level);
+        if (correct >= 5) {
 
-      }
-  }
-  
-  function levelSetting(){
+            level += 1;
+            myStopFunction();
+            correct = 0;
+            document.getElementById('level').innerText = level;
+            document.getElementById('info').innerHTML = "";
 
-      if(level === 1){
+        }
+        localStorage.setItem("score", score);
+        document.getElementById('info').innerHTML = `<span style="color:green;" >CORRECT</span>`;
+        levelSetting();
+        clearField();
 
-          randomWord(wordslv1);
+    } else {
 
-      }
-      else if(level === 2){
+        info.innerHTML = `<span style="color:red";>WRONG</span>`;
+        alert('Oops ....!,Wrong word')
+        window.location.href = "score.html";
+        
+    }
+}
 
-          randomWord(wordslv2);
+function levelSetting() {
 
-      }else {
+    if (level === 1) {
 
-          alert("Congrats...You won the match");
-          window.location.href = "score.html";
+        randomWord(wordslv1);
 
-      }
-       shuffled(randomData);
+    }
+    else if (level === 2) {
 
-  }
-  
-  levelSetting();
+        randomWord(wordslv2);
 
-  function quitButton (){
+    } else {
+
+        alert("Congrats...You won the match");
+        window.location.href = "score.html";
+
+    }
+    shuffled(randomData);
+
+}
+
+levelSetting();
+
+function quitButton() {
 
     window.location.href = 'score.html';
 
 }
 
 
-    
+
 function startTimer(duration, display) {
     var timer = duration, minutes, seconds;
-     myInterval = setInterval(function () {
+    myInterval = setInterval(function () {
         minutes = parseInt(timer / 60, 10)
         seconds = parseInt(timer % 60, 10);
-    
+
         minutes = minutes < 10 ? "0" + minutes : minutes;
         seconds = seconds < 10 ? "0" + seconds : seconds;
         display.textContent = minutes + ":" + seconds;
-        
-        if( minutes === "00" && seconds === "00"){
-            levelSetting();
+
+        if (minutes === "00" && seconds === "00") {
+            alert('Oops...!Time up ')
+            window.location.href = "score.html";
         }
-            if (--timer < 0) {
-                timer = duration;
-            }
-        }, 1000);
-        myInterval
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+    myInterval
 }
 
 function myStopFunction() {
     clearInterval(myInterval);
-  }
-    
-function timerFunction () {
-     var timeMinutes = 70 - (10 * level),
-     display = document.querySelector('#time');
-        
-        startTimer(timeMinutes, display);
-        
-    };
+}
+
+function timerFunction() {
+    var timeMinutes = 70 - (10 * level),
+        display = document.querySelector('#time');
+
+    startTimer(timeMinutes, display);
+
+};
 
 
 
 
 
-  
